b778b5ad9f244e4a621652a8d765fed0df4a053a
Fontaine Sylvain <sfontaine2@tc405-109-09.insa-lyon.fr>
2015-06-16 15:36:46 +0200

    First commit

---
+++ src/test/java/hello/RomanNumberConverterTest.java
@@ -0,0 +1,34 @@
+package hello;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class RomanNumberConverterTest{
+    RomanNumberConverter romanNumberConverter;
+   
+    @Before
+    public void setup() {
+        romanNumberConverter = new RomanNumberConverter();
+    }
+   
+    @Test
+    public void check() {
+        assertThat(romanNumberConverter.convert("M"))
+          .isEqualTo(1000);
+    }
+   
+    @Test
+    public void check_soustra(){
+        assertThat(romanNumberConverter.convert("IV"))
+            .isEqualTo(4);
+        assertThat(romanNumberConverter.convert("IX"))
+            .isEqualTo(9);
+        assertThat(romanNumberConverter.convert("IM"))
+            .isEqualTo(999);
+        assertThat(romanNumberConverter.convert("VX"))
+            .isEqualTo(5);
+    }
+    
+}
